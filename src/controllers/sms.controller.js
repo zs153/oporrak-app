2b18518f564b23f8c2151126bbc1bc750f78a571
zs153 <tmp153@gmail.com>
2022-03-07 14:33:03 +0100

    corr validacion referencia en sms

--- src/controllers/sms.controller.js
+++ src/controllers/sms.controller.js
@@ -1,49 +1,49 @@
-import axios from 'axios'
+import axios from "axios";
 import {
   arrEstadosSms,
   estadosSms,
   tiposMovimiento,
-} from '../public/js/enumeraciones'
+} from "../public/js/enumeraciones";
 
 export const mainPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
   try {
-    const result = await axios.get('http://localhost:8000/api/smss')
+    const result = await axios.get("http://localhost:8000/api/smss");
 
-    const datos = { smss: result.data.dat }
-    res.render('admin/smss', { user, datos })
+    const datos = { smss: result.data.dat };
+    res.render("admin/smss", { user, datos });
   } catch (error) {
-    res.redirect('/')
+    res.redirect("/");
   }
-}
+};
 export const addPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const sms = {
     idsmss: 0,
-    texsms: '',
-    movsms: '',
+    texsms: "",
+    movsms: "",
     stasms: 0,
-  }
+  };
 
   try {
     const datos = {
       sms,
       arrEstadosSms,
-    }
+    };
 
-    res.render('admin/smss/add', { user, datos })
+    res.render("admin/smss/add", { user, datos });
   } catch (error) {
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   }
-}
+};
 export const editPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
   try {
-    const result = await axios.post('http://localhost:8000/api/sms', {
+    const result = await axios.post("http://localhost:8000/api/sms", {
       id: req.params.id,
-    })
+    });
 
     const sms = {
       idsmss: result.data.idsmss,
@@ -51,123 +51,124 @@ export const editPage = async (req, res) => {
       movsms: result.data.movsms,
       stasms: result.data.stasms,
       refdoc: result.data.refdoc,
-    }
+    };
     const datos = {
       sms,
       arrEstadosSms,
-    }
+    };
 
-    res.render('admin/smss/edit', { user, datos })
+    res.render("admin/smss/edit", { user, datos });
   } catch (error) {
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   }
-}
+};
 export const insertSms = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const sms = {
     referencia: req.body.refdoc,
     texto: req.body.texsms,
     movil: req.body.movsms,
     estado: estadosSms.pendiente,
-  }
+  };
   const movimiento = {
     usuarioMov: user.id,
     tipoMov: tiposMovimiento.crearSms,
-  }
+  };
 
   try {
     // referencia
     const result = await axios.post(
-      'http://localhost:8000/api/formularios/referencia',
+      "http://localhost:8000/api/formularios/referencia",
       {
         referencia: sms.referencia,
       }
-    )
+    );
+
     const documento = {
       idDocumento: result.data.iddocu,
-    }
-    await axios.post('http://localhost:8000/api/smss/insert', {
+    };
+    await axios.post("http://localhost:8000/api/smss/insert", {
       sms,
       documento,
       movimiento,
-    })
+    });
 
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   } catch (error) {
-    let msg = 'No se ha podido crear el sms. Verifique la referencia'
+    let msg = "No se ha podido crear el sms. Verifique la referencia";
 
     if (error.response.data.errorNum === 20100) {
-      msg = 'La sms ya existe.'
+      msg = "La sms ya existe.";
     }
     try {
       const datos = {
         sms: req.body,
         arrEstadosSms,
-      }
+      };
 
-      res.render('admin/smss/add', { user, datos, alerts: [{ msg }] })
+      res.render("admin/smss/add", { user, datos, alerts: [{ msg }] });
     } catch (error) {
-      res.redirect('/admin/smss')
+      res.redirect("/admin/smss");
     }
   }
-}
+};
 export const updateSms = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const sms = {
     id: req.body.idsmss,
     texto: req.body.texsms,
     movil: req.body.movsms,
     estado: req.body.stasms,
-  }
+  };
   const movimiento = {
     usuarioMov: user.id,
     tipoMov: tiposMovimiento.modificarSms,
-  }
+  };
   try {
-    const result = await axios.post('http://localhost:8000/api/smss/update', {
+    const result = await axios.post("http://localhost:8000/api/smss/update", {
       sms,
       movimiento,
-    })
+    });
 
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   } catch (error) {
     let msg =
-      'No se han podido modificar los datos del sms. Verifique los datos introducidos'
+      "No se han podido modificar los datos del sms. Verifique los datos introducidos";
 
     if (error.response.data.errorNum === 20100) {
-      msg = 'El sms ya existe'
+      msg = "El sms ya existe";
     }
 
     try {
       const datos = {
         sms: req.body,
         arrEstadosSms,
-      }
+      };
 
-      res.render('admin/smss/edit', { user, datos, alerts: [{ msg }] })
+      res.render("admin/smss/edit", { user, datos, alerts: [{ msg }] });
     } catch (error) {
-      res.redirect('/admin/smss')
+      res.redirect("/admin/smss");
     }
   }
-}
+};
 export const deleteSms = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const sms = {
     id: req.body.idsmss,
-  }
+  };
   const movimiento = {
     usuarioMov: user.id,
     tipoMov: tiposMovimiento.borrarSms,
-  }
+  };
 
   try {
-    const result = await axios.post('http://localhost:8000/api/smss/delete', {
+    const result = await axios.post("http://localhost:8000/api/smss/delete", {
       sms,
       movimiento,
-    })
+    });
 
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   } catch (error) {
-    res.redirect('/admin/smss')
+    res.redirect("/admin/smss");
   }
-}
+};
--- src/routes/sms.router.js
+++ src/routes/sms.router.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import { verifyTokenAndAdmin } from '../middleware/auth'
-import { validateInsert, validateUpdate } from '../middleware/smsValidator'
+import express from "express";
+import { verifyTokenAndAdmin } from "../middleware/auth";
+import { validateInsert, validateUpdate } from "../middleware/smsValidator";
 import {
   mainPage,
   addPage,
@@ -8,55 +8,63 @@ import {
   insertSms,
   updateSms,
   deleteSms,
-} from '../controllers/sms.controller'
-import { check } from 'express-validator'
+} from "../controllers/sms.controller";
+import { check } from "express-validator";
 
-const smsRouter = express.Router()
+const smsRouter = express.Router();
 
 // paginas
-smsRouter.get('/smss', verifyTokenAndAdmin, mainPage)
-smsRouter.get('/smss/add', verifyTokenAndAdmin, addPage)
-smsRouter.get('/smss/edit/:id', verifyTokenAndAdmin, editPage)
+smsRouter.get("/smss", verifyTokenAndAdmin, mainPage);
+smsRouter.get("/smss/add", verifyTokenAndAdmin, addPage);
+smsRouter.get("/smss/edit/:id", verifyTokenAndAdmin, editPage);
 
 // procedures
 smsRouter.post(
-  '/smss/insert',
+  "/smss/insert",
   verifyTokenAndAdmin,
   [
-    check('texsms')
+    check("refdoc")
       .not()
       .isEmpty()
-      .withMessage('Debe introducir el texto sms')
+      .withMessage("Debe introducir una referencia"),
+    check("texsms")
+      .not()
+      .isEmpty()
+      .withMessage("Debe introducir el texto sms")
       .isLength({ min: 1, max: 140 })
-      .withMessage('La longitud del texto mínimo 1 y máximo 140 caracteres'),
-    check('movsms')
+      .withMessage("La longitud del texto máximo 140 caracteres"),
+    check("movsms")
       .isLength({ min: 9, max: 9 })
-      .withMessage('El número de movil debe de tener 9 dígitos')
+      .withMessage("El número de movil debe de tener 9 dígitos")
       .matches(/^([6]{1})([0-9]{8})|(7[1-4]{1})([0-9]{7})$/)
-      .withMessage('Introduzca un número de movil válido'),
+      .withMessage("Introduzca un número de movil válido"),
   ],
   validateInsert,
   insertSms
-)
+);
 smsRouter.post(
-  '/smss/update',
+  "/smss/update",
   verifyTokenAndAdmin,
   [
-    check('texsms')
+    check("refdoc")
       .not()
       .isEmpty()
-      .withMessage('Debe introducir el texto sms')
+      .withMessage("Debe introducir una referencia"),
+    check("texsms")
+      .not()
+      .isEmpty()
+      .withMessage("Debe introducir el texto sms")
       .isLength({ min: 1, max: 140 })
-      .withMessage('La longitud del texto mínimo 1 y máximo 140 caracteres'),
-    check('movsms')
+      .withMessage("La longitud del texto máximo 140 caracteres"),
+    check("movsms")
       .isLength({ min: 9, max: 9 })
-      .withMessage('El número de movil debe de tener 9 dígitos')
+      .withMessage("El número de movil debe de tener 9 dígitos")
       .matches(/^([6]{1})([0-9]{8})|(7[1-4]{1})([0-9]{7})$/)
-      .withMessage('Introduzca un número de movil válido'),
+      .withMessage("Introduzca un número de movil válido"),
   ],
   validateUpdate,
   updateSms
-)
-smsRouter.post('/smss/delete', verifyTokenAndAdmin, deleteSms)
+);
+smsRouter.post("/smss/delete", verifyTokenAndAdmin, deleteSms);
 
-export default smsRouter
+export default smsRouter;
