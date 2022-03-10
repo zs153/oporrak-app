183a0a9454e7bfaedf5d6ca267dce1cea5ba80d0
ZS53 <zs53@telefonica.net>
2022-03-10 21:29:25 +0100

    add ejercicio frau

--- src/routes/fraude.router.js
+++ src/routes/fraude.router.js
@@ -4,6 +4,7 @@ import {
   mainPageFraude,
   addPageFraude,
   editPageFraude,
+  ejercicioPageFraude,
   insertFraude,
   updateFraude,
   deleteFraude,
@@ -15,6 +16,7 @@ import {
   desadjudicarFraude,
   verTodoFraude,
   smsFraude,
+  ejercicioFraude,
 } from '../controllers/fraude.controller'
 
 const fraudeRouter = express.Router()
@@ -23,7 +25,8 @@ const fraudeRouter = express.Router()
 fraudeRouter.get('/fraudes', authRoutes, mainPageFraude)
 fraudeRouter.get('/fraudes/add', authRoutes, addPageFraude)
 fraudeRouter.get('/fraudes/edit/:id', authRoutes, editPageFraude)
-fraudeRouter.get('/fraudes/sms', authRoutes, mainPageFraude)
+fraudeRouter.get('/fraudes/ejercicio/:id', authRoutes, ejercicioPageFraude)
+fraudeRouter.get('/fraudes/vertodo', authRoutes, verTodoFraude)
 
 // procedures
 fraudeRouter.post('/fraudes/insert', authRoutes, insertFraude)
@@ -33,8 +36,8 @@ fraudeRouter.post('/fraudes/asignar', authRoutes, asignarFraude)
 fraudeRouter.post('/fraudes/resolver', authRoutes, resolverFraude)
 fraudeRouter.post('/fraudes/remitir', authRoutes, remitirFraude)
 fraudeRouter.post('/fraudes/desadjudicar', authRoutes, desadjudicarFraude)
-fraudeRouter.get('/fraudes/vertodo', authRoutes, verTodoFraude)
 fraudeRouter.post('/fraudes/sms', authRoutes, smsFraude)
+fraudeRouter.post('/fraudes/ejercicio', authRoutes, ejercicioFraude)
 fraudeRouter.post('/fraudes/cambio', authRoutes, changePasswordFraude)
 fraudeRouter.post('/fraudes/updatePerfil', authRoutes, updatePerfilFraude)
 
