3e059a80b77172b6eec84cae6b8fc19fddf9dca3
ZS53 <zs53@telefonica.net>
2022-03-16 20:03:25 +0100

    se genera estilos.min.css

--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { check } from 'express-validator'
 import {
   loginPage,
   verifyLogin,
@@ -9,38 +8,16 @@ import {
   registroPage,
   crearRegistro,
 } from '../controllers/login.controller'
-import { validate } from '../middleware/registroValidator'
 
 const loginRouter = express.Router()
 
 // auth
 loginRouter.get('/login', loginPage)
-loginRouter.post('/login', verifyLogin)
 loginRouter.get('/logout', verifyLogout)
 loginRouter.get('/forgot', forgotPage)
-loginRouter.post('/forgot', forgotPassword)
 loginRouter.get('/registro', registroPage)
-loginRouter.post(
-  '/registro',
-  [
-    check('userid')
-      .not()
-      .isEmpty()
-      .withMessage('Debe introducir userid')
-      .isLength({ min: 4, max: 20 })
-      .withMessage('La longitud de userID es mínimo 4 y máximo 20'),
-    check('nomusu')
-      .not()
-      .isEmpty()
-      .withMessage('Debe introducir nombre')
-      .isLength({ min: 1, max: 250 })
-      .withMessage('Longitud máxima del nombre es de 250 caracteres'),
-    check('emausu')
-      .isEmail()
-      .withMessage('Debe introducir un correo electrónico'),
-  ],
-  validate,
-  crearRegistro
-)
+loginRouter.post('/login', verifyLogin)
+loginRouter.post('/forgot', forgotPassword)
+loginRouter.post('/registro', crearRegistro)
 
 export default loginRouter
