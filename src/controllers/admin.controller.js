e835ef3a3b37d9d61350c609e141d67818bdb6f9
zs153 <tmp153@gmail.com>
2022-04-20 14:26:15 +0200

    perfil de usuario

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,82 +1,70 @@
-import axios from 'axios'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
-import { tiposMovimiento } from '../public/js/enumeraciones'
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { tiposMovimiento } from "../public/js/enumeraciones";
 
 export const mainPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
-  try {
-    const resultOficinas = await axios.get('http://localhost:8000/api/oficinas')
-
-    const datos = {
-      arrOficinas: resultOficinas.data,
-    }
-    res.render('admin/index', { user, datos })
-  } catch (error) {
-    const msg = 'No se ha podido acceder a los datos de la aplicación.'
-
-    res.render('admin/error400', {
-      alerts: [{ msg }],
-    })
-  }
-}
+  res.render("admin/index", { user });
+};
 export const estadisticaPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
-  res.render('admin/estadistica', { user })
-}
+  res.render("admin/estadistica", { user });
+};
 export const acumuladosPage = async (req, res) => {
-  const user = req.user
+  const user = req.user;
 
-  res.render('admin/estadistica/acumulados', { user })
-}
+  res.render("admin/estadistica/acumulados", { user });
+};
 export const errorPage = async (req, res) => {
-  res.render('admin/error400')
-}
+  res.render("admin/error400");
+};
 // proc
 export const estadistica = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const periodo = {
     desde: req.body.desde,
     hasta: req.body.hasta,
-  }
+  };
 
   const result = await axios.post(
-    'http://localhost:8000/api/formularios/estadistica',
+    "http://localhost:8000/api/formularios/estadistica",
     { periodo }
-  )
+  );
 
-  res.render('admin/estadistica/acumulados', { user, datos: result.data })
-}
+  res.render("admin/estadistica/acumulados", { user, datos: result.data });
+};
 export const changePassword = async (req, res) => {
-  const user = req.user
-  const salt = await bcrypt.genSalt(10)
-  const passHash = await bcrypt.hash(req.body.pwdusu, salt)
+  const user = req.user;
+  const salt = await bcrypt.genSalt(10);
+  const passHash = await bcrypt.hash(req.body.pwdusu, salt);
   const usuario = {
     idusua: req.body.idusua,
     pwdusu: passHash,
-  }
+  };
   const movimiento = {
     usumov: user.id,
     tipmov: tiposMovimiento.cambioPassword,
-  }
+  };
+
   try {
     const result = await axios.post(
-      'http://localhost:8000/api/usuarios/cambio',
+      "http://localhost:8000/api/usuarios/cambio",
       {
         usuario,
         movimiento,
       }
-    )
+    );
 
-    res.redirect('/admin')
+    res.redirect("/admin");
   } catch (error) {
-    res.redirect('/admin')
+    res.redirect("/admin");
   }
-}
+};
 export const updatePerfil = async (req, res) => {
-  const user = req.user
+  const user = req.user;
   const usuario = {
     id: user.id,
     userid: req.body.userid,
@@ -85,17 +73,17 @@ export const updatePerfil = async (req, res) => {
     rol: user.rol,
     oficina: req.body.ofiusu,
     telefono: req.body.telusu,
-  }
+  };
   const movimiento = {
     usuarioMov: user.id,
     tipoMov: tiposMovimiento.modificarPerfil,
-  }
+  };
 
   try {
-    await axios.post('http://localhost:8000/api/formularios/updatePerfil', {
+    await axios.post("http://localhost:8000/api/formularios/updatePerfil", {
       usuario,
       movimiento,
-    })
+    });
 
     const accessToken = jwt.sign(
       {
@@ -108,22 +96,22 @@ export const updatePerfil = async (req, res) => {
         telefono: usuario.telefono,
       },
       `${process.env.ACCESS_TOKEN_SECRET}`,
-      { expiresIn: '8h' }
-    )
+      { expiresIn: "8h" }
+    );
     const options = {
-      path: '/',
+      path: "/",
       sameSite: true,
       maxAge: 1000 * 60 * 60 * 8, // 8 horas
       httpOnly: true,
-    }
+    };
 
-    res.cookie('auth', accessToken, options)
-    res.redirect('/admin/formularios')
+    res.cookie("auth", accessToken, options);
+    res.redirect("/admin/formularios");
   } catch (error) {
-    const msg = 'No se ha podido actualizar el perfil de usuario'
+    const msg = "No se ha podido actualizar el perfil de usuario";
 
-    res.render('admin/error400', {
+    res.render("admin/error400", {
       alerts: [{ msg }],
-    })
+    });
   }
-}
+};
