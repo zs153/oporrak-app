7509f5d1a920208619f376f6fe1f5c36f0e1d167
ZS53 <zs53@telefonica.net>
2022-05-22 19:27:37 +0200

    corregir textos de presentacion

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
-
 export const secret = process.env.ACCESS_TOKEN_SECRET;
 export const maxRows = 50000;
 export const batchSize = 1000;
