d80ea167ee085c37e64a7fd13df239ee89edc80d
zs153 <tmp153@gmail.com>
2022-05-30 14:29:26 +0200

    upd estadistica fraude,formularios y citas

--- src/routes/estadisticas.router.js
+++ src/routes/estadisticas.router.js
@@ -1,10 +1,16 @@
 import express from "express";
 import authRoutes from "../middleware/auth";
-import { fraudePage } from "../controllers/estadistica.controller";
+import { citasPage, estadisticaCita, estadisticaFormulario, estadisticaFraude, formularioPage, fraudePage } from "../controllers/estadistica.controller";
 
 const estadisticaRouter = express.Router();
 
 // paginas
 estadisticaRouter.get("/estadisticas/fraudes", authRoutes, fraudePage);
+estadisticaRouter.get("/estadisticas/formularios", authRoutes, formularioPage);
+estadisticaRouter.get("/estadisticas/citas", authRoutes, citasPage);
+// proc
+estadisticaRouter.post("/estadisticas/fraudes", authRoutes, estadisticaFraude);
+estadisticaRouter.post("/estadisticas/formularios", authRoutes, estadisticaFormulario);
+estadisticaRouter.post("/estadisticas/formularios", authRoutes, estadisticaCita);
 
 export default estadisticaRouter;
