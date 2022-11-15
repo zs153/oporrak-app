e3cfa0500c7272f4fa8b1d33a5f06ba1ebed79a9
zs153 <tmp153@gmail.com>
2022-11-15 16:33:40 +0100

    upd url

--- src/controllers/traspaso.controller.js
+++ src/controllers/traspaso.controller.js
@@ -18,7 +18,6 @@ export const mainPage = async (req, res) => {
 
     res.render('admin/traspasos', { user, datos })
   } catch (error) {
-    console.log(error)
     const msg = 'No se ha podido acceder a los datos de la aplicación.'
 
     res.render('admin/error400', {
