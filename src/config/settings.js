0cdb0b506d28df804938e3b0097600d480865d34
ZS53 <zs53@telefonica.net>
2023-01-06 18:57:37 +0100

    upd

--- src/config/settings.js
+++ src/config/settings.js
@@ -6,8 +6,8 @@ export const dbPool = {
   user: process.env.NODE_ORACLEDB_USER,
   password: process.env.NODE_ORACLEDB_PASSWORD,
   connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING,
-  poolMin: 4,
-  poolMax: 64,
+  poolMin: 0,
+  poolMax: 4,
   poolIncrement: 1,
 }
 export const maxRows = 50000
