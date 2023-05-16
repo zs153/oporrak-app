69a87eff4c8c75140967b79fb9c8a623b2575ae5
zs153 <tmp153@gmail.com>
2023-05-16 18:59:24 +0200

    upd

--- src/services/web-server.js
+++ src/services/web-server.js
@@ -69,14 +69,3 @@ function close() {
 }
 
 module.exports.close = close
-
-const iso8601RegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/
-
-function reviveJson(key, value) {
-  // revive ISO 8601 date strings to instances of Date
-  if (typeof value === 'string' && iso8601RegExp.test(value)) {
-    return new Date(value)
-  } else {
-    return value
-  }
-}
