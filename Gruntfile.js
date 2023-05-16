21bdebafa861e76891e1ed8d74eef635322849fb
zs153 <tmp153@gmail.com>
2023-05-16 18:57:08 +0200

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,8 +24,8 @@ module.exports = function (grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       js: {
-        src: ['./src/views/admin/cursos/turnos/usuarios/addUsuariosTurno.js'],
-        dest: './src/public/js/addUsuariosTurno.min.js'
+        src: ['./src/app.js'],
+        dest: './src/app.min.js'
       },
       css: {
         src:  'src/public/css/concat.css',
