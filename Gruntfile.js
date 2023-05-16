ab7c2a4397e5d4ba46c54b63641aa0778b503799
zs153 <tmp153@gmail.com>
2023-05-16 13:00:41 +0200

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,8 +24,8 @@ module.exports = function (grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       js: {
-        src: ['./src/public/js/oficina.js'],
-        dest: './src/public/js/oficina.min.js'
+        src: ['./src/views/user/estados/semanal.js'],
+        dest: './src/public/js/semanal.min.js'
       },
       css: {
         src:  'src/public/css/concat.css',
